766034054e22d86a34b70640d3a01c8890d191ff
Jonathan Ostrander <jonathanost@gmail.com>
2019-12-13 09:10:08 -0500

    day 13 part 1

---
+++ aoc/src/main/scala/Problem13.scala
@@ -0,0 +1,50 @@
+package aoc
+
+object Problem13 extends App {
+  println(
+    LazyList.continually(0L).scanLeft(
+      (
+        scala.io.Source.fromResource("inputs/13.txt").getLines.next.split(",")
+          .map(_.toLong)
+          .zipWithIndex
+          .map(t => t._2.toLong -> t._1)
+          .toMap.withDefaultValue(0L) ++ Map(0L -> 2L),
+        0L,
+        true,
+        List.empty[Long],
+        0L
+      )
+    )((a, input) =>
+      Some("%05d".format(a._1(a._2)).zipWithIndex.map(_.swap).toMap).map(cm =>
+        Some((m: Char) =>
+          if (m == '0') ((i: Long) => a._1(a._1(i)), (i: Long) => a._1(i))
+          else if (m == '1') ((i: Long) => a._1(i), (i: Long) => i)
+          else if (m == '2') ((i: Long) => a._1(a._5 + a._1(i)), (i: Long) => a._5 + a._1(i))
+          else sys.error("bad mode input")
+        ).map(f => cm -> (0 to 2).map(i => f(cm(i)))).get
+      ).map(t =>
+        if (t._1(3) == '9' && t._1(4) == '9') (a._1, a._2, false, a._4, a._5)
+        else if (t._1(4) == '1') (a._1 ++ Map(t._2(0)._2(a._2 + 3L) -> (t._2(2)._1(a._2 + 1L) + t._2(1)._1(a._2 + 2L))), a._2 + 4L, true, a._4, a._5)
+        else if (t._1(4) == '2') (a._1 ++ Map(t._2(0)._2(a._2 + 3L) -> (t._2(2)._1(a._2 + 1L) * t._2(1)._1(a._2 + 2L))), a._2 + 4L, true, a._4, a._5)
+        else if (t._1(4) == '3') (a._1 ++ Map(t._2(2)._2(a._2 + 1L) -> input), a._2 + 2L, true, a._4, a._5)
+        else if (t._1(4) == '4') (a._1, a._2 + 2, true, t._2(2)._1(a._2 + 1L) :: a._4, a._5)
+        else if (t._1(4) == '5') (a._1, if (t._2(2)._1(a._2 + 1L) != 0L) t._2(1)._1(a._2 + 2L) else a._2 + 3, true, a._4, a._5)
+        else if (t._1(4) == '6') (a._1, if (t._2(2)._1(a._2 + 1L) == 0L) t._2(1)._1(a._2 + 2L) else a._2 + 3, true, a._4, a._5)
+        else if (t._1(4) == '7') (a._1 ++ Map(t._2(0)._2(a._2 + 3L) -> (if (t._2(2)._1(a._2 + 1L) < t._2(1)._1(a._2 + 2L)) 1L else 0L)), a._2 + 4L, true, a._4, a._5)
+        else if (t._1(4) == '8') (a._1 ++ Map(t._2(0)._2(a._2 + 3L) -> (if (t._2(2)._1(a._2 + 1L) == t._2(1)._1(a._2 + 2L)) 1L else 0L)), a._2 + 4L, true, a._4, a._5)
+        else if (t._1(4) == '9') (a._1, a._2 + 2, a._3, a._4, a._5 + t._2(2)._1(a._2 + 1L))
+        else sys.error(s"bad op code input ${t._1}")
+      ).get
+    )
+      .dropWhile(_._3)
+      .head
+      ._4
+      .reverse
+      .sliding(3, 3)
+      .toList
+      .foldLeft[Map[(Long, Long), Long]](Map.empty)((m, l) => m ++ Map((l(0), l(1)) -> l(2)))
+      .toList
+      .filter(_._2 == 2)
+      .length
+  )
+}
